3373787b67c56e605f2141301fd031c50bc9649e
fegala <31254488+fegala@users.noreply.github.com>
2017-11-10 13:48:18 -0600

    Add files via upload
    Problema 3 del examen

---
+++ Problema3.cpp
@@ -0,0 +1,15 @@
+//Fernando Garza Landa
+#include <iostream>
+using namespace std;
+int main(){
+float d;
+float t;
+float v;
+cout<<"Distancia "<<endl;//Le damos valor a la d
+cin>>d;
+cout<<"Tiempo "<<endl;//Le damos valor a la t
+cin>>t;
+v=d/t;//se hace la división correspondiente para dar la velocidad v
+cout<< "La velocidad es " <<v <<endl;//Se imprime el valor de la velocidad
+return 0;
+}
